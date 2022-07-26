2526ee030dcd84cd1e91a7736c9f36fac6063e14
root <root@localhost>
2022-07-26 15:53:23 -0700

    [Update] 0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 pattern = /School/
 argstr = ARGF.argv[0].inspect
-result = argstr.scan(pattern).flatten
-for i in result
+results = argstr.scan(pattern).flatten
+for i in results
 	print i
 end
 if not i.nil?

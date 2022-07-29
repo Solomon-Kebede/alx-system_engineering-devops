e8137ab1f2155c3a69a6be7ab70775711007d713
root <root@localhost>
2022-07-29 11:47:51 -0700

    [Update#2] 100-textme.rb

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,7 +3,7 @@ pattern = /from:(.*)\W{3}to:(.*)\W{3}flags:(.*)\W{3}msg/
 argstr = ARGF.argv[0].inspect
 result = argstr.scan(pattern).flatten
 len = result.length
-puts len
+#puts len
 for i in result
 	print i
 	if (result.find_index(i)) != (len - 1)

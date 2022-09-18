46fab89b9057a475564dae9acbb019b3f65f2509
root <root@localhost>
2022-09-18 13:21:30 -0700

    Update-2: Task 4. Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,10 +7,6 @@
 #     Your SSH client configuration must be configured to use the private key `~/.ssh/school`
 #     Your SSH client configuration must be configured to refuse to authenticate using a password
 
-# ensure private key exists
-file {'~/.ssh/school':
-  ensure => file
-}
 
 # Turn off passwd auth
 exec {'Turn off passwd auth':
@@ -20,6 +16,7 @@ exec {'Turn off passwd auth':
 
 # Declare identity file
 exec {'Declare identity file':
+  require  => Exec['Turn off passwd auth'],
   command  => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
   provider => 'shell'
 }

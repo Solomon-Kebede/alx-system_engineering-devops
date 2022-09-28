325f7a7dcdb9a062f01f73097d73d909e8d54822
root <root@localhost>
2022-09-27 17:51:48 -0700

    Task 2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,46 @@
+# execute 'apt-get update'
+exec { 'apt-update':
+  command  => 'sudo /usr/bin/apt-get update',
+  provider => 'shell'
+}
+
+# install nginx package
+package { 'nginx':
+  require => Exec['apt-update'],
+  ensure  => installed
+}
+
+# set directory permissions for file writing
+exec { 'chmod /var/www':
+  require  => Package['nginx'],
+	command  => '/usr/bin/chmod -R 777 /var/www',
+	provider => 'shell'
+}
+
+# create homepage
+file { '/var/www/html/index.html':
+  require => Exec['chmod /var/www'],
+  ensure  => file,
+  content => "Hello World!"
+}
+
+# start nginx service
+service { 'nginx start':
+  require  => File['/var/www/html/index.html'],
+	ensure   => running,
+	provider => 'service'
+}
+
+# edit server response header
+exec { 'edit-header':
+  require  => Service['nginx start'],
+	command  => '/usr/bin/sed -i 's/404;/404;add_header X-Served-By $hostname;/g' /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+# restart nginx service
+exec { 'nginx restart':
+  require  => Exec['edit-headerx'],
+  command  => 'sudo /usr/sbin/service nginx restart'
+	provider => 'shell'
+}

af15df485f14011a6aedeabbaf680dea4e37fbc2
Solomon Kebede <solomonaau188@gmail.com>
2022-11-10 17:58:22 +0000

    Task 0. Sky is the limit, let's bring that limit higher

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+exec { 'from_4_to_10-workers':
+  command  => "sed -i s/'worker_processes 4'/'worker_processes 10'/g /etc/nginx/nginx.conf",
+  provider => 'shell',
+  user     => 'root'
+}
+
+
+exec { 'service nginx restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+  user     => 'root'
+}

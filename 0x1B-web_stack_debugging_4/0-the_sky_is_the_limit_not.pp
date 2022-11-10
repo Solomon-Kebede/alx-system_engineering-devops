60f661a33224e6a9b540d7b96a4929999e2ce9cf
Solomon Kebede <solomonaau188@gmail.com>
2022-11-10 18:14:51 +0000

    [Task 0] Add comments

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,6 @@
+# Replace the number of workers from 4 to 10
+# Restart nginx server
+
 exec { 'from_4_to_10-workers':
   command  => "sed -i s/'worker_processes 4'/'worker_processes 10'/g /etc/nginx/nginx.conf",
   provider => 'shell',

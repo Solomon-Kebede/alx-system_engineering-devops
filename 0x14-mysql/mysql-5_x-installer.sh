b78a061e94f267acf8ee4666a3ead872a610a9fc
solomon Kebede <solomonaau188@gmail.com>
2022-12-16 16:16:22 +0000

    Task 0. Install MySQL [MySQL 5.7 Installer]

---
+++ 0x14-mysql/mysql-5_x-installer.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+sudo apt-key add signature.key
+
+# add the apt repo
+sudo sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
+
+#update apt
+sudo apt-get update
+
+#now check your available versions:
+sudo apt-cache policy mysql-server
+
+#Now install mysql 5.7
+sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*

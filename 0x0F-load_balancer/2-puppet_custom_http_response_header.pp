f50a22f4551c3998502b6374bfc2e9b5c1e20f01
Solomon Kebede <solomonaau188@gmail.com>
2022-12-17 21:45:06 +0000

    Task 2. Add a custom HTTP header with Puppet [ADV] (Fix commands part 2)

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,3 +1,9 @@
+# Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppet.
+#   The name of the custom HTTP header must be X-Served-By
+#   The value of the custom HTTP header must be the hostname of the server Nginx is running on
+#   Write 2-puppet_custom_http_response_header.pp so that it configures a brand new Ubuntu machine to the requirements asked in this task
+
+
 # execute 'apt-get update'
 exec { 'apt-update':
   command  => '/usr/bin/sudo /usr/bin/apt-get update',
@@ -7,11 +13,10 @@ exec { 'apt-update':
 # install nginx package
 package { 'nginx':
   require => Exec['apt-update'],
-  ensure  => installed
 }
 
 # set directory permissions for file writing
-exec { 'chmod /var/www':
+exec { 'chmod-/var/www':
   require  => Package['nginx'],
 	command  => '/usr/bin/sudo /usr/bin/chmod -R 777 /var/www',
 	provider => 'shell'
@@ -19,28 +24,28 @@ exec { 'chmod /var/www':
 
 # create homepage
 file { '/var/www/html/index.html':
-  require => Exec['chmod /var/www'],
+  require => Exec['chmod-/var/www'],
   ensure  => file,
   content => "Hello World!"
 }
 
 # start nginx service
-service { 'nginx start':
+exec { 'nginx-start':
   require  => File['/var/www/html/index.html'],
-	ensure   => running,
-	provider => 'service'
+	command  => '/usr/bin/sudo /usr/sbin/nginx',
+	provider => 'shell'
 }
 
 # edit server response header
 exec { 'edit-header':
-  require  => Service['nginx start'],
-	command  => '/usr/bin/sudo /usr/bin/sed -i "s/404;/404;add_header X-Served-By $hostname;/g" /etc/nginx/sites-available/default',
+  require  => Exec['nginx-start'],
+	command  => '/usr/bin/sudo /usr/bin/sed -i "s/404;/404;add_header X-Served-By $(uname -n);/g" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
 # restart nginx service
-exec { 'nginx restart':
-  require  => Exec['edit-headerx'],
-  command  => '/usr/bin/sudo /usr/sbin/nginx -s stop; /usr/bin/sudo /usr/sbin/nginx'
+exec { 'nginx-restart':
+  require  => Exec['edit-header'],
+  command  => '/usr/bin/sudo /usr/sbin/nginx -s stop; /usr/bin/sudo /usr/sbin/nginx',
 	provider => 'shell'
 }
